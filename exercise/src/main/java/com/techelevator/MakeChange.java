71f74e9b512ef7bb423dfef9b5b4f1627049b909
Micah McLain <mclainmicah1@gmail.com>
2023-05-19 17:31:43 -0400

    M1W1 team exercise

--- exercise/src/main/java/com/techelevator/MakeChange.java
+++ exercise/src/main/java/com/techelevator/MakeChange.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 /*
  Write a command line program which prompts the user for the total bill, and the amount tendered. It should then
  display the change required.
@@ -11,7 +13,25 @@ package com.techelevator;
 public class MakeChange {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner (System.in);
+		System.out.println("Welcome to the Restaurant!");
+		System.out.println("PLease enter the receipt total:");
+		String receipt = scanner.nextLine();
+		double receiptTotal = Double.parseDouble(receipt);
 
+		System.out.println("Please enter amount paid:");
+		String paid = scanner.nextLine();
+		double paidTotal = Double.parseDouble(paid);
+
+		if(paidTotal < receiptTotal) {
+			System.out.println("Please give me more money");
+		}
+		if(paidTotal == receiptTotal) {
+			System.out.println("You give no change");
+		}
+		if (paidTotal > receiptTotal) {
+			System.out.println("The change required is " + (paidTotal - receiptTotal));
+		}
 	}
 
 }
--- exercise/src/main/java/com/techelevator/MartianWeight.java
+++ exercise/src/main/java/com/techelevator/MartianWeight.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 /*
 In case you've ever pondered how much you weigh on Mars, here's the calculation:
  	Wm = We * 0.378
@@ -17,7 +19,23 @@ Enter a series of Earth weights (space-separated): 98 235 185
 public class MartianWeight {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner (System.in);
+		System.out.println("Please enter a series of Earth weights separated by spaces:");
+		String userInput = scanner.nextLine();
+		String[] earthArray = userInput.split(" ");
+		//double marsWeight = 0;
+		for (int i = 0; i < earthArray.length; i++) {
+			int earthWeight = Integer.parseInt(earthArray[i]);
+			int marsWeight = (int)(earthWeight * 0.378);
+			if(earthWeight < 0) {
+				System.out.println("Weight cannot be less than 0.");
+				//break;
+			}
+			if(earthWeight >= 0) {
+				System.out.println(earthWeight + "lbs. on Earth is " + marsWeight + "lbs. on Mars.");
+			}
 
+		}
 	}
 
 }
